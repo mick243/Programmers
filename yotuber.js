feb2135d93f242e3e82ede76e06f143dd219878f
mick243 <39043252+mick243@users.noreply.github.com>
2024-04-29 12:29:17 +0900

    Update yotuber.js

--- yotuber.js
+++ yotuber.js
@@ -27,13 +27,14 @@ let id = 0
 db.set(++id, youtuber1)
 db.set(++id, youtuber2)
 db.set(++id, youtuber3)
+//id 값을 변수로 지정 0부터 시작해 등록할때마다 전위증가 시켜서 1,2,3...순서대로 등록
 
 app.use(express.json())
 app.post('/youtubers', function (req, res) {  
     db.set(++id, req.body)
     res.json({
         message :`${req.body.channelTitle}님 유튜버 생활을 시작해보세요~!`
-    })
+    }) // post 받은 body값의 channelTitle을 그대로 출력
 })
 
 
